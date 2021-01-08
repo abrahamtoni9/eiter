50e897c036e30e06824c4489d23145a6cf22ad59
Abraham Salomon <abrahamtoni9@hotmail.com>
2021-01-07 21:19:36 -0300

    Comienzo del proyecto

---
+++ dbproyecto.sql
@@ -0,0 +1,556 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-01-2021 a las 01:16:24
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbproyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `categoria`, `estado`, `id_usuario`) VALUES
+(1, 'Receptora', '0', 3),
+(2, 'Comestibles', '1', 3),
+(3, 'Embutidos', '0', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `cedula_cliente` varchar(100) NOT NULL,
+  `nombre_cliente` varchar(100) NOT NULL,
+  `apellido_cliente` varchar(100) NOT NULL,
+  `telefono_cliente` varchar(100) NOT NULL,
+  `correo_cliente` varchar(100) NOT NULL,
+  `direccion_cliente` varchar(100) NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `cedula_cliente`, `nombre_cliente`, `apellido_cliente`, `telefono_cliente`, `correo_cliente`, `direccion_cliente`, `fecha_ingreso`, `estado`, `id_usuario`) VALUES
+(1, '3692439', 'Abraham', 'Salomon', '0961747071', 'abrahamtoni9@hotmail.com', '6511 NW 87th Ave, #CLI220600', '2021-01-06', '0', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `id_compras` int(11) NOT NULL,
+  `fecha_compra` date NOT NULL,
+  `numero_compra` varchar(100) NOT NULL,
+  `proveedor` varchar(100) NOT NULL,
+  `cedula_proveedor` varchar(100) NOT NULL,
+  `comprador` varchar(100) NOT NULL,
+  `moneda` varchar(100) NOT NULL,
+  `subtotal` varchar(100) NOT NULL,
+  `total_iva` varchar(100) NOT NULL,
+  `total` varchar(100) NOT NULL,
+  `tipo_pago` varchar(100) NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `compras`
+--
+
+INSERT INTO `compras` (`id_compras`, `fecha_compra`, `numero_compra`, `proveedor`, `cedula_proveedor`, `comprador`, `moneda`, `subtotal`, `total_iva`, `total`, `tipo_pago`, `estado`, `id_usuario`, `id_proveedor`) VALUES
+(1, '2020-12-31', 'F000001', 'RUBEN SALOMON', '24324324', 'Tadeo', 'USD$', '500', '100', '600', 'EFECTIVO', '0', 3, 1),
+(2, '2020-12-31', 'F000002', 'RUBEN SALOMON', '24324324', 'Tadeo', 'USD$', '2000', '400', '2400', 'EFECTIVO', '1', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_compras`
+--
+
+CREATE TABLE `detalle_compras` (
+  `id_detalle_compras` int(11) NOT NULL,
+  `numero_compra` varchar(100) NOT NULL,
+  `cedula_proveedor` varchar(100) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `producto` varchar(100) NOT NULL,
+  `moneda` varchar(100) NOT NULL,
+  `precio_compra` varchar(100) NOT NULL,
+  `cantidad_compra` varchar(100) NOT NULL,
+  `descuento` varchar(100) NOT NULL,
+  `importe` varchar(100) NOT NULL,
+  `fecha_compra` date NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `detalle_compras`
+--
+
+INSERT INTO `detalle_compras` (`id_detalle_compras`, `numero_compra`, `cedula_proveedor`, `id_producto`, `producto`, `moneda`, `precio_compra`, `cantidad_compra`, `descuento`, `importe`, `fecha_compra`, `id_usuario`, `id_proveedor`, `estado`, `id_categoria`) VALUES
+(1, 'F000001', '24324324', 1, 'Mostaza', 'USD$', '500', '1', '0', '500', '2020-12-31', 3, 1, '0', 1),
+(2, 'F000002', '24324324', 2, 'mayoneza', 'USD$', '500', '3', '0', '1500', '2020-12-31', 3, 1, '1', 1),
+(3, 'F000002', '24324324', 3, 'sabo', 'USD$', '500', '1', '0', '500', '2020-12-31', 3, 1, '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `id_detalle_ventas` int(11) NOT NULL,
+  `numero_venta` varchar(100) NOT NULL,
+  `cedula_cliente` varchar(100) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `producto` varchar(100) NOT NULL,
+  `moneda` varchar(100) NOT NULL,
+  `precio_venta` varchar(100) NOT NULL,
+  `cantidad_venta` varchar(100) NOT NULL,
+  `descuento` varchar(100) NOT NULL,
+  `importe` varchar(100) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `estado` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `id_empresa` int(11) NOT NULL,
+  `cedula_empresa` varchar(100) NOT NULL,
+  `nombre_empresa` varchar(100) NOT NULL,
+  `direccion_empresa` varchar(100) NOT NULL,
+  `correo_empresa` varchar(100) NOT NULL,
+  `telefono_empresa` varchar(100) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id_permiso` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `producto` varchar(100) NOT NULL,
+  `presentacion` varchar(100) NOT NULL,
+  `unidad` varchar(45) NOT NULL,
+  `moneda` varchar(45) NOT NULL,
+  `precio_compra` varchar(45) NOT NULL,
+  `precio_venta` varchar(45) NOT NULL,
+  `stock` varchar(45) NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `imagen` varchar(45) NOT NULL,
+  `fecha_vencimiento` date NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `id_categoria`, `producto`, `presentacion`, `unidad`, `moneda`, `precio_compra`, `precio_venta`, `stock`, `estado`, `imagen`, `fecha_vencimiento`, `id_usuario`) VALUES
+(1, 1, 'Mostaza', 'Saco', 'kilo', 'USD$', '500', '800', '0', '1', '461149056.jpg', '2021-06-22', 3),
+(2, 1, 'mayoneza', 'Caja', 'kilo', 'USD$', '500', '800', '3', '1', '', '2021-06-22', 3),
+(3, 1, 'sabo', 'Caja', 'kilo', 'USD$', '500', '800', '1', '1', '1636412743.jpg', '2020-12-31', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
+  `cedula` varchar(45) NOT NULL,
+  `razon_social` varchar(100) NOT NULL,
+  `telefono` varchar(100) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `direccion` varchar(150) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id_proveedor`, `cedula`, `razon_social`, `telefono`, `correo`, `direccion`, `fecha`, `estado`, `id_usuario`) VALUES
+(1, '24324324', 'RUBEN SALOMON', '2435546546', 'rubenmanuel@hotmail.com', '22 de setiembre 1170 y eusebio ayala', '2020-12-29', '1', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `cedula` varchar(100) NOT NULL,
+  `telefono` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `cargo` enum('0','1') NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `password2` varchar(100) NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `estado` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombres`, `apellidos`, `cedula`, `telefono`, `correo`, `direccion`, `cargo`, `usuario`, `password`, `password2`, `fecha_ingreso`, `estado`) VALUES
+(1, 'Jose', 'Salomon', '3454544', '45654654', 'josesalo@hotmail.com', 'jose verdun c/ herrera', '0', 'jose', 'Qw/*12345678', 'Qw/*12345678', '2020-03-03', '0'),
+(2, 'Abraham', 'Salomon', '3692439', '3054146488', 'abrahamtoni9@hotmail.com', '6511 NW 87th Ave, #CLI220600', '1', 'abraham', 'Qw/*12345678', 'Qw/*12345678', '2020-12-19', '1'),
+(3, 'Tadeo', 'Benitez', '4503234', '0981438203', 'tadeo@hotmail.com', 'Gral Aquino c/ Herrera ', '0', 'tadeo', '123', '123', '2020-12-19', '1'),
+(4, 'Edson', 'Salomon', '5345345', '0981438203', 'edson@hotmail.com', '6511 NW 87th Ave, #CLI220600', '1', 'admin', '123', '123', '2020-12-25', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_permiso`
+--
+
+CREATE TABLE `usuario_permiso` (
+  `id_usuario_permiso` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_permiso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_ventas` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `numero_venta` varchar(100) NOT NULL,
+  `cliente` varchar(100) NOT NULL,
+  `cedula_cliente` varchar(100) NOT NULL,
+  `vendedor` varchar(100) NOT NULL,
+  `moneda` varchar(100) NOT NULL,
+  `subtotal` varchar(100) NOT NULL,
+  `total_iva` varchar(100) NOT NULL,
+  `total` varchar(100) NOT NULL,
+  `tipo_pago` varchar(100) NOT NULL,
+  `estado` enum('0','1') NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`),
+  ADD KEY `fk_categoria_usuarios_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `fk_clientes_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id_compras`),
+  ADD KEY `fk_compras_usuario_idx` (`id_usuario`),
+  ADD KEY `fk_compras_proveedor_idx` (`id_proveedor`);
+
+--
+-- Indices de la tabla `detalle_compras`
+--
+ALTER TABLE `detalle_compras`
+  ADD PRIMARY KEY (`id_detalle_compras`),
+  ADD KEY `fk_detalle_compras_producto_idx` (`id_producto`),
+  ADD KEY `fk_detalle_compras_usuario_idx` (`id_usuario`),
+  ADD KEY `fk_detalle_compras_proveedor_idx` (`id_proveedor`),
+  ADD KEY `fk_detalle_compras_categoria_idx` (`id_categoria`);
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`id_detalle_ventas`),
+  ADD KEY `fk_detalle_ventas_producto_idx` (`id_producto`),
+  ADD KEY `fk_detalle_ventas_usuario_idx` (`id_usuario`),
+  ADD KEY `fk_detalle_ventas_clientes_idx` (`id_cliente`);
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id_empresa`),
+  ADD KEY `fk_empresa_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id_permiso`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `fk_producto_categoria_idx` (`id_categoria`),
+  ADD KEY `fk_producto_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`),
+  ADD KEY `fk_proveedor_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD PRIMARY KEY (`id_usuario_permiso`),
+  ADD KEY `fk_usuario_permiso_usuario_idx` (`id_usuario`),
+  ADD KEY `fk_usuario_permiso_permiso_idx` (`id_permiso`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_ventas`),
+  ADD KEY `fk_ventas_usuario_idx` (`id_usuario`),
+  ADD KEY `fk_ventas_cliente_idx` (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id_compras` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_compras`
+--
+ALTER TABLE `detalle_compras`
+  MODIFY `id_detalle_compras` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  MODIFY `id_detalle_ventas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id_permiso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  MODIFY `id_usuario_permiso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_ventas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD CONSTRAINT `fk_categoria_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `fk_clientes_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `fk_compras_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_compras_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_compras`
+--
+ALTER TABLE `detalle_compras`
+  ADD CONSTRAINT `fk_detalle_compras_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_compras_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_compras_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_compras_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `fk_detalle_ventas_clientes` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ventas_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ventas_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD CONSTRAINT `fk_empresa_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_producto_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD CONSTRAINT `fk_proveedor_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD CONSTRAINT `fk_usuario_permiso_permiso` FOREIGN KEY (`id_permiso`) REFERENCES `permisos` (`id_permiso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_permiso_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `fk_ventas_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ventas_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
